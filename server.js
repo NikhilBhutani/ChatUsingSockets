7ec8137cf04399378f2c1693d6908b8839818f44
NikhilBhutani <niks.bhutani@gmail.com>
2017-01-15 01:00:57 -0800

    completed chat with socket.io

--- server.js
+++ server.js
@@ -21,19 +21,27 @@ io.sockets.on('connection', function(socket){
 
 //Disconnect
  socket.on('disconnect', function(data){
+   users.splice(users.indexOf(socket.username),1);
+   updateUserNames();
    connections.splice(connections.indexOf(socket), 1);
    console.log('Disconnected: %s sockets connected', connections.length);
  });
 
 //Sending a message
 socket.on('send message', function(data){
-   io.sockets.emit('new message', {msg:data});
+   io.sockets.emit('new message', {msg:data, user: socket.username});
 });
 
+socket.on('new user', function(data, callback){
+   callback(true);
+   socket.username = data;
+   users.push(socket.username);
+   updateUserNames();
 });
 
+function updateUserNames()
+{
+   io.sockets.emit('get users', users);
+ }
 
-// //Closing a connection with socket.io
-// io.sockets.on('disconnect', function(socket){
-//
-// });
+});
